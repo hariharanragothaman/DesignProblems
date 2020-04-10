b55db6348b37b0284a7e163f894a9a5c706407ac
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2020-04-10 01:41:46 -0400

    Wrapping up the constants class

--- Amazon/constants.py
+++ Amazon/constants.py
@@ -2,17 +2,34 @@ from enum import Enum
 
 class Address:
     def __init__(self, street, city, state, country, zipcode):
+        self.street = street
+        self.city = city
+        self.state = state
+        self.country = country
+        self.zipcode = zipcode
 
 class OrderStatus(Enum):
-    NOT_SHIPPED = 0
-    PENDING = 1
-    SHIPPED = 2
-    COMPLETED = 3
-    CANCELLED = 4
-    REFUND_APPLIED = 5
+    NOT_SHIPPED = 1
+    PENDING = 2
+    SHIPPED = 3
+    COMPLETED = 4
+    CANCELLED = 5
+    REFUND_APPLIED = 6
 
 class ShipmentStatus(Enum):
-    PENDING = 0
-    SHIPPED = 1
-    DELIVERED = 2
-    ON_HOLD =  3
+    PENDING = 1
+    SHIPPED = 2
+    DELIVERED = 3
+    ON_HOLD =  4
+
+class PaymentStatus(Enum):
+    UNPAID = 1
+    PENDING = 2
+    COMPLETED = 3
+    FILLED = 4
+    DECLINED = 5
+    CANCELLED = 6
+    ABANDONED = 7
+    SETTLING = 8
+    SETTLED = 9
+    REFUNDED = 10
